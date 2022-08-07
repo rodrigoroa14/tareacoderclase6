c097ed0cea8a0f6d8b095d0ef027c5003d7f69de
Rodrigo <rodrigo.roa14@gmail.com>
2022-08-07 15:45:49 -0300

    archivo env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const  {Productos} = require('./productos.js')
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 
 app.get('/', (req, res) => {
